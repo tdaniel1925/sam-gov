f1d0e3302f48c11159d59d3c5638dc496d559d69
tdaniel1925 <trenttdaniel@gmail.com>
2026-01-29 01:44:25 -0600

    Switch back to ProfessionalApp with new simple Dashboard

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import ProfessionalApp from './ProfessionalApp.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ProfessionalApp />
   </React.StrictMode>
 );
-// Build: 1769671440 - Fixed to use App.tsx with environment variable checks
+// Build: 1769672000 - Using ProfessionalApp with new Dashboard
