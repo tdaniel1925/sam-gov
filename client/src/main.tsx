0d9f94e3878d9b90e772ee909939e0a09fa20d2d
tdaniel1925 <trenttdaniel@gmail.com>
2026-01-29 01:47:12 -0600

    Force new bundle with timestamp and console log

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ProfessionalApp from './ProfessionalApp.tsx';
 import './index.css';
 
+// FORCE NEW BUNDLE - Build: 2026-01-29-07:45:00
+console.log('🔥 NEW BUILD LOADED - 2026-01-29-07:45:00');
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ProfessionalApp />
-  </React.StrictMode>
+  <ProfessionalApp />
 );
-// Build: 1769672000 - Using ProfessionalApp with new Dashboard
